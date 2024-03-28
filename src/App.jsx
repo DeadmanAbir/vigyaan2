d9bfc177927629a136110217973346b3e36021af
Ujjawal Saini <spignelon@protonmail.com>
2024-03-29 01:21:35 +0530

    Added FAQ Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import Hero from "../Components/Hero";
 import Speakers from "../Components/Speakers";
 import Package from "../Components/Package";
+import Accordion from "../Components/Accordian";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,9 +12,9 @@ function App() {
   return (
     <>
       <Hero />
-
       <Speakers />
       <Package />
+      <Accordion/>
     </>
   );
 }
