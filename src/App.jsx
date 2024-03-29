fc8a040cebbe5a35e93186b546625aed2a73f221
Mohammad-416 <149521106+Mohammad-416@users.noreply.github.com>
2024-03-29 14:45:08 +0530

    Revert "Merge pull request #2 from DeadmanAbir/speakers_wpsadi"
    This reverts commit 6ef0ad496e7752495d2072a9201fb6cec2055f6f, reversing
    changes made to 12535b67018c69ad65d83a37f8c998cbab981f34.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import Hero from "../Components/Hero";
 import Speakers from "../Components/Speakers";
 import Package from "../Components/Package";
-import Accordion from "../Components/Accordian";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <Hero />
+
       <Speakers />
       <Package />
-      <Accordion/>
     </>
   );
 }
