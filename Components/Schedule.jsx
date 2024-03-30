5c9eee11eaebd72838d99206f1e501d0b29e82f2
Rajdeep paul <112900557+rajdeeppaul304@users.noreply.github.com>
2024-03-30 16:14:43 +0530

    Update Schedule.jsx

--- Components/Schedule.jsx
+++ Components/Schedule.jsx
@@ -1,121 +1,44 @@
-
+    
 
 function Schedule(){
     return (<>
     <section id="Schedule">
-    <div className="bg-[#0c0c0c]" >
+    <div className="bg-[#001736]" >
       <div className="flex justify-center py-12 px-10">
       <img src="../src/assets/schedule.jpg" alt="" />
       </div>
-      <div className="py-10 bg-[#0c0c0c] flex justify-center">
+      <div className="py-10 bg-[#001736] flex justify-center">
 
-        <a href="../src/assets/schedule.gif" download className="bg-[#1B1A55] hover:bg-[#535C91] hover:text-white hover:border-[#1B1A55] text-[#9290C3] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline border border-[#535C91] ">
+        <a href="../src/assets/schedule.jpg" download className="bg-[#000] hover:bg-[#155e89] hover:text-white hover:border-[#1B1A55] text-[#9290C3] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline border border-[#535C91] ">
           Download Schedule
         </a>
       </div>
     </div>
 
-    <div id="events" className="wow fadeInUp relative overflow-hidden pt-17.5 lg:pt-22.5 xl:pt-27.5 bg-[#0c0c0c]">
+    <div id="events" className="wow fadeInUp relative overflow-hidden pt-17.5 lg:pt-22.5 xl:pt-27.5 bg-[#001736] text-[#fff]">
       <div className="max-w-[1170px] mx-auto px-4 sm:px-8 xl:px-0">
         <div className="max-w-full mx-auto px-4 my-5 sm:px-8 xl:px-0">
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 sm:gap-7.5 ">
-            <div className="wow fadeInUp group bg-[#1B1A55] backdrop-filter backdrop-blur-sm rounded-lg outline outline-offset-1 outline-[#535C91]" style={{ visibility: 'visible' }}>
+
+          <div className="wow fadeInUp group bg-[#03396c] backdrop-filter backdrop-blur-sm rounded-lg " style={{ visibility: 'visible' }}>
               <div className="overflow-hidden rounded-xl ">
-                <img loading="lazy" src="../images/gold.png" alt="image" className="ease-linear w-full duration-500 scale-100 group-hover:scale-125 h-80" />
+                <img loading="lazy" src="../src/assets/speaker.jpg" alt="image" className="ease-linear w-full duration-500 scale-100 group-hover:scale-125 h-80" />
               </div>
 
               <div className="p-4 h-60 flex flex-col justify-between">
-                <h4 className="text-[#9290C3] font-semibold text-4xl ease-in duration-300 hover:opacity-80">
+                <h4  className="red-hat">
                   Inauguration
                 </h4>
-                <p className="font-medium text-[#9290C3]">
+                <p className="font-medium opacity-80">
                   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur inventore distinctio dignissimos, qui aut sequi delectus deleniti velit alias suscipit provident possimus cumque, placeat et! Sunt iste rem fugit laborum?
                 </p>
 
 
               </div>
             </div>
-            <div className="wow fadeInUp group bg-[#1B1A55] backdrop-filter backdrop-blur-sm rounded-lg outline outline-offset-1 outline-[#535C91]" style={{ visibility: 'visible' }}>
-              <div className="overflow-hidden rounded-xl ">
-                <img loading="lazy" src="../images/gold.png" alt="image" className="ease-linear w-full duration-500 scale-100 group-hover:scale-125 h-80" />
-              </div>
-
-              <div className="p-4 h-60 flex flex-col justify-between">
-                <h4 className="text-[#9290C3] font-semibold text-4xl ease-in duration-300 hover:opacity-80">
-                  Inauguration
-                </h4>
-                <p className="font-medium text-[#9290C3]">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur inventore distinctio dignissimos, qui aut sequi delectus deleniti velit alias suscipit provident possimus cumque, placeat et! Sunt iste rem fugit laborum?
-                </p>
 
 
-              </div>
-            </div>
-            <div className="wow fadeInUp group bg-[#1B1A55] backdrop-filter backdrop-blur-sm rounded-lg outline outline-offset-1 outline-[#535C91]" style={{ visibility: 'visible' }}>
-              <div className="overflow-hidden rounded-xl ">
-                <img loading="lazy" src="../images/gold.png" alt="image" className="ease-linear w-full duration-500 scale-100 group-hover:scale-125 h-80" />
-              </div>
-
-              <div className="p-4 h-60 flex flex-col justify-between">
-                <h4 className="text-[#9290C3] font-semibold text-4xl ease-in duration-300 hover:opacity-80">
-                  Inauguration
-                </h4>
-                <p className="font-medium text-[#9290C3]">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur inventore distinctio dignissimos, qui aut sequi delectus deleniti velit alias suscipit provident possimus cumque, placeat et! Sunt iste rem fugit laborum?
-                </p>
-
-
-              </div>
-            </div>
-            <div className="wow fadeInUp group bg-[#1B1A55] backdrop-filter backdrop-blur-sm rounded-lg outline outline-offset-1 outline-[#535C91]" style={{ visibility: 'visible' }}>
-              <div className="overflow-hidden rounded-xl ">
-                <img loading="lazy" src="../images/gold.png" alt="image" className="ease-linear w-full duration-500 scale-100 group-hover:scale-125 h-80" />
-              </div>
-
-              <div className="p-4 h-60 flex flex-col justify-between">
-                <h4 className="text-[#9290C3] font-semibold text-4xl ease-in duration-300 hover:opacity-80">
-                  Inauguration
-                </h4>
-                <p className="font-medium text-[#9290C3]">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur inventore distinctio dignissimos, qui aut sequi delectus deleniti velit alias suscipit provident possimus cumque, placeat et! Sunt iste rem fugit laborum?
-                </p>
-
-
-              </div>
-            </div>
-            <div className="wow fadeInUp group bg-[#1B1A55] backdrop-filter backdrop-blur-sm rounded-lg outline outline-offset-1 outline-[#535C91]" style={{ visibility: 'visible' }}>
-              <div className="overflow-hidden rounded-xl ">
-                <img loading="lazy" src="../images/gold.png" alt="image" className="ease-linear w-full duration-500 scale-100 group-hover:scale-125 h-80" />
-              </div>
-
-              <div className="p-4 h-60 flex flex-col justify-between">
-                <h4 className="text-[#9290C3] font-semibold text-4xl ease-in duration-300 hover:opacity-80">
-                  Inauguration
-                </h4>
-                <p className="font-medium text-[#9290C3]">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur inventore distinctio dignissimos, qui aut sequi delectus deleniti velit alias suscipit provident possimus cumque, placeat et! Sunt iste rem fugit laborum?
-                </p>
-
-
-              </div>
-            </div>
-            <div className="wow fadeInUp group bg-[#1B1A55] backdrop-filter backdrop-blur-sm rounded-lg outline outline-offset-1 outline-[#535C91]" style={{ visibility: 'visible' }}>
-              <div className="overflow-hidden rounded-xl ">
-                <img loading="lazy" src="../images/gold.png" alt="image" className="ease-linear w-full duration-500 scale-100 group-hover:scale-125 h-80" />
-              </div>
-
-              <div className="p-4 h-60 flex flex-col justify-between">
-                <h4 className="text-[#9290C3] font-semibold text-4xl ease-in duration-300 hover:opacity-80">
-                  Inauguration
-                </h4>
-                <p className="font-medium text-[#9290C3]">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur inventore distinctio dignissimos, qui aut sequi delectus deleniti velit alias suscipit provident possimus cumque, placeat et! Sunt iste rem fugit laborum?
-                </p>
-
-
-              </div>
-            </div>
 
 
           </div>
